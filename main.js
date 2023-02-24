f8483cb1c4330b7f7cd6f74fc7f9fd92b6877c31
Arpon <a4arpon@gmail.com>
2023-02-24 23:27:06 +0600

    Type: Janina Author -> Arpon  Changes to be committed: 	new file:   .vscode/launch.json 	modified:   index.html 	new file:   main.js

---
+++ main.js
@@ -0,0 +1,64 @@
+airEngine = async (city) => {
+  let response = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=1a38bbe1a56f4e3899e93626232402&q=${city}&aqi=yes`,
+    {
+      method: "POST",
+    }
+  );
+
+  let data = await response.json();
+  printer(data);
+};
+printer = (data) => {
+  idArray = [
+    "name",
+    "country",
+    "tz_id",
+    "temp_c",
+    "localtime",
+    "uv",
+    "wind_kph",
+    "wind_dir",
+    "cloud",
+    "co",
+    "o3",
+    "no2",
+    "so2",
+  ];
+  for (let i = 0; i < idArray.length; i++) {
+    let output = data["location"][idArray[i]];
+    if (output !== undefined) {
+      document.getElementById(idArray[i]).innerText = output;
+    } else {
+      let output = data["current"][idArray[i]];
+      document.getElementById(idArray[i]).innerText = output;
+      if (output == undefined) {
+        let output = data["current"]["air_quality"][idArray[i]];
+        document.getElementById(idArray[i]).innerText = parseInt(output);
+      }
+    }
+  }
+  airQ = parseInt(data["current"]["air_quality"]["us-epa-index"]);
+  switch (airQ) {
+    case 1:
+      document.getElementById("us-epa-index").innerText = "Good";
+      break;
+    case 2:
+      document.getElementById("us-epa-index").innerText = "Moderate";
+      break;
+    case 3:
+      document.getElementById("us-epa-index").innerText =
+        "Unhealthy for Sensitive";
+      break;
+    case 4:
+      document.getElementById("us-epa-index").innerText = "Unhealthy";
+      break;
+    case 5:
+      document.getElementById("us-epa-index").innerText = "Very Unhealthy";
+      break;
+    case 6:
+      document.getElementById("us-epa-index").innerText = "Hazardous";
+      break;
+  }
+};
+airEngine("Dhaka");
